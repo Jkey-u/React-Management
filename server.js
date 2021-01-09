474291002f1c0e4ec0bc8e832fb2d86f9ab5cead
Jkey-u <dum6894@gmail.com>
2021-01-09 22:25:00 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,27 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id': 1,
-            'image': 'http://placeimg.com/64/64/1',
-            'name': '류재경',
-            'birthday': '940426',
-            'gender': '여자',
-            'job': '대학생'
-        },
-        {
-            'id': 2,
-            'image': 'http://placeimg.com/64/64/2',
-            'name': '김성규',
-            'birthday': '951119',
-            'gender': '남자',
-            'job': '회시원'
-        },
-        {
-            'id': 3,
-            'image': 'http://placeimg.com/64/64/3',
-            'name': '제이키',
-            'birthday': '940426',
-            'gender': '여자',
-            'job': '회시원'
-        }
-    ]);
+    //res.send();
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fields) => {
+            res.send(rows);
+        }  
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
